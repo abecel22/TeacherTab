249b000c04b1a84ca1a5d8c5814add0ec0d1cd64
abecel22 <abe2211@gmail.com>
2017-11-25 21:42:52 -0600

    Added background with Unslpash.

---
+++ dist/js/background.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var body = document.querySelector("body").style;
+var img = 'https://source.unsplash.com/user/abe2211/likes/1600x900';
+
+body.background = 'url(\'' + img + '\') no-repeat fixed center';
+body.backgroundSize = 'cover';
---
+++ src/js/background.js
@@ -0,0 +1,6 @@
+
+const body = document.querySelector("body").style;
+const img =   'https://source.unsplash.com/user/abe2211/likes/1600x900';
+
+body.background = `url('${img}') no-repeat fixed center`;
+body.backgroundSize = 'cover'; 
