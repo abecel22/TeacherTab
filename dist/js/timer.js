1026c803a656bb4bc661e23e6fb7c452d8e41546
abecel22 <abe2211@gmail.com>
2017-11-30 20:47:47 -0600

    Added transitions and toggle for timer view

--- dist/js/timer.js
+++ dist/js/timer.js
@@ -2,9 +2,16 @@
 
 var timerButton = document.querySelector('#timerButton');
 var timerDiv = document.querySelector('.timerDiv');
+var conatiner = document.querySelector('.container');
 
 function viewTimer() {
-    timerDiv.style.display = 'table';
+    if (timerDiv.style.display === 'table') {
+        timerDiv.style.display = 'none';
+        conatiner.style.background = 'initial';
+    } else {
+        timerDiv.style.display = 'table';
+        conatiner.style.background = 'rgba(43, 49, 38, 0.45)';
+    }
 }
 
 timerButton.addEventListener('click', viewTimer);
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,8 +1,15 @@
 const timerButton = document.querySelector('#timerButton');
 const timerDiv = document.querySelector('.timerDiv');
+const conatiner = document.querySelector('.container');
 
 function viewTimer() {
-    timerDiv.style.display = 'table';
+    if(timerDiv.style.display === 'table') {
+        timerDiv.style.display = 'none';
+        conatiner.style.background = 'initial';
+    } else {
+        timerDiv.style.display = 'table';
+        conatiner.style.background = 'rgba(43, 49, 38, 0.45)';
+    } 
 }
 
 timerButton.addEventListener('click', viewTimer);
