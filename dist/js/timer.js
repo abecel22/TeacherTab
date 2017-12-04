8a665b108f41086d66483b0e071792120a296a21
abecel22 <abe2211@gmail.com>
2017-12-03 21:34:49 -0600

    Added countdown for timer with JS

--- dist/js/timer.js
+++ dist/js/timer.js
@@ -1,1 +1,21 @@
-"use strict";
+'use strict';
+
+var countdown = void 0;
+var totalSeconds = void 0;
+var timerValue = document.querySelector('.timerVal');
+var startButton = document.querySelector('#startButton');
+
+function runTimer() {
+    totalSeconds = 60;
+    countdown = setInterval(function () {
+        totalSeconds--;
+        displayTimeLeft();
+    }, 1000);
+}
+
+function displayTimeLeft() {
+
+    timerValue.textContent = '00:' + totalSeconds;
+}
+
+startButton.addEventListener('click', runTimer);
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,3 +1,20 @@
+let countdown;
+let totalSeconds;
+let timerValue = document.querySelector('.timerVal');
+const startButton = document.querySelector('#startButton');
 
 
+function runTimer() {
+    totalSeconds = 60;
+    countdown = setInterval(() => {
+        totalSeconds--;
+        displayTimeLeft();
+    }, 1000);
+}
 
+function displayTimeLeft() {
+    
+    timerValue.textContent = `00:${totalSeconds}`;
+}
+
+startButton.addEventListener('click', runTimer);
