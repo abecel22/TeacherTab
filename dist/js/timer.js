0886ee88e4a375f3f6183fd1549870de68e280b6
abecel22 <abe2211@gmail.com>
2017-12-08 23:30:21 -0600

    Added timer submit time button

--- dist/js/timer.js
+++ dist/js/timer.js
@@ -1,27 +1,42 @@
 'use strict';
 
-var countdown = void 0;
 var minutes = void 0;
-var secondsRemaining = 180;
+var countdown = void 0;
+var secondsRemaining = void 0;
 var timerValue = document.querySelector('.timerVal');
 var startButton = document.querySelector('#startButton');
+var resetButton = document.querySelector('#resetButton');
+var enterTime = document.customForm;
 
-function runTimer() {
-
+function runTimer(secondsRemaining) {
     countdown = setInterval(function () {
         secondsRemaining--;
-        displayTimeLeft();
+        displayTimeLeft(secondsRemaining);
     }, 1000);
 }
 
-function displayTimeLeft() {
+function displayTimeLeft(secondsRemaining) {
     var min = Math.floor(secondsRemaining / 60);
     var sec = secondsRemaining % 60;
     if (sec < 10) {
-        timerValue.textContent = min + ' : 0' + sec;
+        timerValue.textContent = min + ':0' + sec;
     } else {
-        timerValue.textContent = min + ' : ' + sec;
+        timerValue.textContent = min + ':' + sec;
     }
 }
 
+function getTime(e) {
+    e.preventDefault();
+    minutes = this.minutes.value;
+    secondsRemaining = minutes * 60;
+    runTimer(secondsRemaining);
+    this.reset();
+}
+
+function resetTimer() {
+    clearInterval(countdown);
+}
+
 startButton.addEventListener('click', runTimer);
+resetButton.addEventListener('click', resetTimer);
+enterTime.addEventListener('submit', getTime);
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,29 +1,42 @@
-let countdown;
 let minutes;
-let secondsRemaining = 180;
+let countdown;
+let secondsRemaining;
 let timerValue = document.querySelector('.timerVal');
 const startButton = document.querySelector('#startButton');
+const resetButton = document.querySelector('#resetButton');
+const enterTime = document.customForm;
 
 
 
-function runTimer() {
-    
+function runTimer(secondsRemaining) {
     countdown = setInterval(() => {
         secondsRemaining--;
-        displayTimeLeft();
+        displayTimeLeft(secondsRemaining);
     }, 1000);
 }
 
-function displayTimeLeft() {
+function displayTimeLeft(secondsRemaining) {
     let min = Math.floor(secondsRemaining / 60);
     let sec = secondsRemaining % 60;
     if(sec < 10) {
-        timerValue.textContent = `${min} : 0${sec}`;
+        timerValue.textContent = `${min}:0${sec}`;
     } else {
-        timerValue.textContent = `${min} : ${sec}`;
-    }
-    
+        timerValue.textContent = `${min}:${sec}`;
+    }  
 }
 
+function getTime(e) {
+    e.preventDefault();
+    minutes = this.minutes.value;
+    secondsRemaining = minutes * 60;
+    runTimer(secondsRemaining);
+    this.reset();
+}
+
+function resetTimer() {
+    clearInterval(countdown);
+}
 
 startButton.addEventListener('click', runTimer);
+resetButton.addEventListener('click', resetTimer);
+enterTime.addEventListener('submit', getTime);
